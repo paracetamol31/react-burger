1bcffdedec31f37a44e7254ebbc2dad7f2d40df3
paracetamol31 <ilya.hotchenkov@gmail.com>
2023-11-04 04:10:04 +0300

    sprint-5/step-1 Ревлизован компонент для отображения ингредиентов в ленте заказа

--- src/components/images-row/images-row.tsx
+++ src/components/images-row/images-row.tsx
@@ -1,15 +1,37 @@
+import { RoundImage } from "../round-image/round-image";
 import imagesRowStyles from "./images-row.module.css";
 
 import { FC } from "react";
 
 export interface ImagesRowProps {
-    images: Array<string>
+    images: Array<string>,
+    quantityShowedImages?: number
 }
 
-export const ImagesRow: FC<ImagesRowProps> = (props) => {
+export const ImagesRow: FC<ImagesRowProps> = ({ images, quantityShowedImages = 6 }) => {
+    const roundImages: Array<JSX.Element> = [];
+    if (images.length < quantityShowedImages) {
+        quantityShowedImages = images.length
+    }
+    for (let i = 0; i < quantityShowedImages; i++) {
+        roundImages.push(
+            <>
+                <RoundImage size={64} src={images[i]} index={quantityShowedImages - i} key={i} extraStyle={{ right: `${i * 20}px` }}>
+                    {(i === quantityShowedImages - 1 && quantityShowedImages < images.length) &&
+                        <div
+                            className={`${imagesRowStyles.numberRemainingElements} text text_type_main-small`}
+                        >
+                            +{images.length - quantityShowedImages}
+                        </div>}
+                    
+                </RoundImage>
+
+            </>
+        );
+    }
     return (
         <section className={imagesRowStyles.wrapper}>
-
+            {roundImages}
         </section>
     )
 }
